93f0a11b830e700a9483a2daff839294a9ad3064
Matthew Reynolds <9809557+matt-re@users.noreply.github.com>
2026-05-12 09:31:30 +0100

    Read expression from args

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include "rpn.h"
 
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	std::string expr;
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			if (i > 1) {
+				expr += ' ';
+			}
+			expr += argv[i];
+		}
+	} else {
+		expr = "2 3 + 10 *";
+	}
 
-	std::optional<double> result = rpn_calc("2 3 + 10 *");
+	std::optional<double> result = rpn_calc(expr);
 	if (result) {
 		std::cout << *result << "\n";
 		return 0;
